f1e41d2098a8a8441586d30e3b3f80b12c5819c5
rajashree-n <rajashreenaik14@gmail.com>
2016-05-01 18:24:49 +0530

    adding comments

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 
 function login(){
-
+//taking i/p
 var username = document.getElementById("usernameid").value;
 var password = document.getElementById("passwordid").value;
 
+//checking for null values
 if(username === "" || password === ""){
 document.getElementById("loginfail").style.display="block";
 return false;
 }
 
+//request-xml
 var xmlReq = new XMLHttpRequest();
 
 xmlReq.open("POST", "https://moodshare.herokuapp.com/login", true); 
@@ -16,7 +18,7 @@ xmlReq.setRequestHeader("Access-Control-Allow-Origin", "*");
 xmlReq.setRequestHeader("Content-type","application/x-www-form-urlencoded");
 xmlReq.send("username="+username+"&password="+password); 
 
-
+//result of req
 xmlReq.onreadystatechange = function(){
 if(xmlReq.readyState == 4 && xmlReq.status == 202){ 
 document.getElementById("loginresult").style.display="block";
